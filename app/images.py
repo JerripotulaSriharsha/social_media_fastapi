3fa5c1e2c16eeb71f1ad1091a3003eaf1fbef709
Sri Harsha Jerripotula <harsha.jerripotula@gmail.com>
2025-12-11 23:05:49 -0600

    changes

---
+++ app/images.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from imagekitio import ImageKit
+import os
+
+load_dotenv()
+
+imagekit = ImageKit(
+    private_key=os.getenv("IMAGE_KIT_PRIVATE_KEY"),
+    public_key=os.getenv("IMAGE_KIT_PUBLIC_KEY"),
+    url_endpoint=os.getenv("IMAGE_KIT_URL"),
+)
+
+
